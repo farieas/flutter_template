12c8fb90400468cf0d9bdf81f3a380e8861c7045
farhan <farhankdi@gmail.com>
2025-10-09 21:18:55 +0530

    Comment out Flutter verification commands in project setup script

--- idx-template.nix
+++ idx-template.nix
@@ -14,10 +14,13 @@
     export PATH=$HOME/flutter/bin:$PATH
     export PATH=$HOME/.pub-cache/bin:$PATH
 
+    # Debugging: Print PATH to verify
+    echo $PATH
+
     # Verify Flutter
     echo "Flutter path being used:"
-    which flutter
-    flutter --version
+    # which flutter
+    # flutter --version
 
     # Create new Flutter project with static name
     echo "Creating Flutter project..."
